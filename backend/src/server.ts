d028da86cf11ac554a30289620fe6a6a7d78137b
dheeran-kumarasamy <dheeran.kumarasamy@gmail.com>
2026-02-22 15:30:10 +0000

    frontend backend link

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
 import { testDbConnection } from "./config/db";
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4000;
 
 async function startServer() {
   try {
